25020675489d06dc79852c08a7887b59ad64bdb2
Leonardo Diehl <leonardodiehl1@gmail.com>
2021-04-18 16:59:26 -0300

    Remove arrayBasedSet unneeded methods reinstancing
    Signed-off-by: Leonardo Diehl <leonardodiehl1@gmail.com>

--- set/arrayBasedSet.js
+++ set/arrayBasedSet.js
@@ -1,45 +1,52 @@
+function entries() {
+  const entries = []
+  for (const value of this) {
+    entries.push([value, value])
+  }
+  return entries
+}
+
+function has(value) {
+  return this.some((item) => item === value)
+}
+
+function add(value) {
+  if (!this.has(value)) {
+    this.push(value)
+  }
+}
+
+function customDelete(value) {
+  const itemIndex = this.findIndex((item) => item === value)
+  if (itemIndex === -1) {
+    return
+  }
+
+  this.splice(itemIndex, 1)
+}
+
+function clear() {
+  this.splice(0)
+}
+
+const sizeDefinition = {
+  enumerable: true,
+  get: function() {
+    return this.length
+  },
+}
+
 function ArrayBasedSet(initialValue) {
   const baseArr = new Array(...initialValue)
 
-  baseArr.entries = function() {
-    const entries = []
-    for (const value of this) {
-      entries.push([value, value])
-    }
-    return entries
-  }
-
-  baseArr.has = function(value) {
-    return this.some((item) => item === value)
-  }
-
-  baseArr.add = function(value) {
-    if (!this.has(value)) {
-      this.push(value)
-    }
-  }
-
-  baseArr.delete = function(value) {
-    const itemIndex = this.findIndex((item) => item === value)
-    if (itemIndex === -1) {
-      return
-    }
-
-    this.splice(itemIndex, 1)
-  }
-
-  baseArr.clear = function() {
-    this.splice(0)
-  }
-
+  baseArr.entries = entries
+  baseArr.has = has
+  baseArr.add = add
+  baseArr.delete = customDelete
+  baseArr.clear = clear
   baseArr.keys = baseArr.values
 
-  Object.defineProperty(baseArr, 'size', {
-    enumerable: true,
-    get: function() {
-      return this.length
-    }
-  })
+  Object.defineProperty(baseArr, 'size', sizeDefinition)
 
   return baseArr
 }
